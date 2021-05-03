97e436ba073de353a035be50908b23ced4b559fd
0debug <54331369+0debug@users.noreply.github.com>
2021-05-03 16:50:10 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import subprocess, requests, time, os
+
+hwid = subprocess.check_output('wmic csproduct get uuid').decode().split('\n')[1].strip()
+r = requests.get('your url here')
+
+try:
+    if hwid in r.text:
+        pass
+    else:
+        print('[ERROR] HWID Not recognized!')
+        print(f'HWID: {hwid}') 
+        time.sleep(5)
+        os._exit()
+except:
+    print('[ERROR] Failed to connect to the database')
+    time.sleep(5) 
+    os._exit() 
+    
+os.system('title HWID Authentication')
+
+print('Welcome')
+input()
