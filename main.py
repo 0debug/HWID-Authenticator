9c3cf5f8cc8f02a4bbd9094c30e7709f55d8208b
0debug <54331369+0debug@users.noreply.github.com>
2021-08-20 16:46:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import subprocess, requests, time, os
 
 hwid = subprocess.check_output('wmic csproduct get uuid').decode().split('\n')[1].strip()
-r = requests.get('your url here')
+r = requests.get('YOUR HWID LIST HERE')
  
 try:
     if hwid in r.text:
         pass
     else:
-        print('[ERROR] HWID Not recognized!')
+        print('[ERROR] Your HWID is not recognized!')
         print(f'HWID: {hwid}') 
-        time.sleep(5)
+        time.sleep(10)
         os._exit()
 except:
-    print('[ERROR] Failed to connect to the database')
-    time.sleep(5) 
+    print('[ERROR] Failed to fetch HWID')
+    time.sleep(10) 
     os._exit() 
     
-os.system('title HWID Authentication')
+os.system('title HWID Authenticator')
 
-print('Welcome')
+print('Complete')
 input()
